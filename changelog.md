90a4f149f82b738cf87eb211c8ad636ba9a6973e
Neil Miller <neil@pie.co.de>
2023-07-25 16:37:32 +0100

    v1.0.1

--- changelog.md
+++ changelog.md
@@ -6,6 +6,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.0.1]
+### Changed
+- Redirect to admin base url
+### Added
+- Redirect URL is now filterable
+
 ## [1.0.0]
 ### Added
 - First commit
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
-$redirect_url = (!is_user_logged_in()) ? wp_login_url() : apply_filters('rta_redirect', admin_url( 'admin.php' ));
+$redirect_url = (!is_user_logged_in()) ? wp_login_url() : apply_filters('rta_redirect', admin_url());
 wp_redirect( $redirect_url );
 exit();
